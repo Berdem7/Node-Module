0dc909ba4ea56ec79b8b29cba37a0f8ae048c9c2
mstars_lab2_07 <mstars_lab2_07@mstars-lab2-07s-iMac.local>
2022-04-14 17:37:08 +0800

    Update

--- Webserver/server.js
+++ Webserver/server.js
@@ -4,15 +4,7 @@ var fs = require("fs");
 http
   .createServer(function (request, response) {
     response.writeHead(200);
-    fs.readFile("index.html", (error, data) => {
-      if (error) {
-        throw error;
-      } else {
-        console.log("Operation success");
-        console.log(data);
-        response.end();
-      }
-    });
+    
 
     // response.write("<h1>Hello Node!!!</h1>\n");
   })
