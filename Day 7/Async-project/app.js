8e03c00c980de99037561bf0828d50f2ebd96cf4
mstars_lab2_07 <mstars_lab2_07@mstars-lab2-07s-iMac.local>
2022-04-27 17:29:23 +0800

    Day 7

---
+++ Day 7/Async-project/app.js
@@ -0,0 +1,16 @@
+const request = require("request");
+
+function printFilms(str, callback) {
+  request(
+    "https://ghibliapi.herokuapp.com/films",
+    function (error, response, body) {
+      //   console.error("error:", error); // Print the error if one occurred
+      //   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      // console.log("body:", body); // Print the HTML for the Google homepage.
+      //   console.log(str);
+      callback(console.log(str));
+    }
+  );
+}
+
+module.exports = printFilms;
---
+++ Day 7/Async-project/async.js
@@ -0,0 +1,10 @@
+const printFilms = require("./app");
+
+function printAll() {
+  printFilms("A");
+  printFilms("B");
+  printFilms("C");
+  // console.log("Hello");
+}
+
+printAll();
